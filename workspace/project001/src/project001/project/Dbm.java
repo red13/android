726a5978bb4c80a9498c473f4ea5afb3c61d5ef0
ryo <code.red.13@gmail.com>
2010-07-09 02:25:46 +0900

    first use Toast and Log.e

--- workspace/project001/src/project001/project/Dbm.java
+++ workspace/project001/src/project001/project/Dbm.java
@@ -1,31 +1,76 @@
 package project001.project;
 
+import android.util.*;
+import android.content.Context;
 import android.database.*;
 import android.database.sqlite.*;
 
+
+
+
 public class Dbm{
 	private SQLiteDatabase db;
-
 	private enum DbmStatus{
 		Opened,
 		Closed
 	};
 	private DbmStatus status = DbmStatus.Closed;
-	
 	private enum DbmDBExistance{
 		Exist,
 		NoExist
 	};
 	private DbmDBExistance db_existance = DbmDBExistance.NoExist;
+	public static String DATABASE_NAME = "Dbm.db";
+	public static int DATABASE_VERSION = 1;
+	private static Context context;
 
-	public Dbm(){
+
+	private static class DbmOpenHelper extends SQLiteOpenHelper{
+		/**
+		 * constructor
+		 * @param ctx context
+		 */
+		public DbmOpenHelper(Context ctx){
+			super(ctx, DATABASE_NAME, null, DATABASE_VERSION );
+		}
+
+		/**
+		 * create table
+		 */
+		@Override
+		public void onCreate(SQLiteDatabase db){
+			// create database
+//			db_existance = DbmDBExistance.Exist;
+			try{
+				db.execSQL("CREATE TABLE ");
+			}catch(Exception e){
+				Log.e("aa","bb");
+			}
+		}
+
+		/**
+		 * update
+		 */
+		@Override
+		public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+		}
+	}
+	private DbmOpenHelper OpenHelper;
+
+	public Dbm(Context ctx){
 		db = null;
 		status = DbmStatus.Closed;
 		db_existance = DbmDBExistance.NoExist;
+		context = ctx;
 	}
 	
-	public void open(){
+	public boolean open(){
+		// create OpenHelper
+		OpenHelper = new DbmOpenHelper( context );
+		db = OpenHelper.getWritableDatabase();
 		status = DbmStatus.Opened;
+		return (db != null);
 	}
 	
 	public void close(){
@@ -39,12 +84,7 @@ public class Dbm{
 			return null;
 		}
 	}
-	public void onCreate(SQLiteDatabase db){
-		// create database
-		db_existance = DbmDBExistance.Exist;
-	}
-	
-	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
-		// not supported
-	}
+
+
 }
+
--- workspace/project001/src/project001/project/project001.java
+++ workspace/project001/src/project001/project/project001.java
@@ -1,8 +1,11 @@
 package project001.project;
 
+import android.util.*;
 import android.app.Activity;
 import android.os.Bundle;
 import project001.project.Dbm;
+import android.widget.*;
+import java.lang.*;
 
 public class project001 extends Activity {
     /** Called when the activity is first created. */
@@ -10,9 +13,13 @@ public class project001 extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
-        Dbm dbm = new Dbm();
+        Log.e("tag","message");
+        Dbm dbm = new Dbm(getBaseContext());
+        Log.e("tag","open");
         dbm.open();
+        dbm.query("create table test-table (_ID Integer auto inclemental)");
         dbm.close();
+        Toast.makeText(getBaseContext(), "toast-test", Toast.LENGTH_LONG).show();
         
         setContentView(R.layout.main);
     }
