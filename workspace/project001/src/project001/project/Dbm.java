1e868d5f4ce9c88c95cea0362a0e07172f34eae8
ryo <code.red.13@gmail.com>
2010-10-30 11:15:09 +0900

    executed raw query sql.

--- workspace/project001/src/project001/project/Dbm.java
+++ workspace/project001/src/project001/project/Dbm.java
@@ -41,11 +41,13 @@ public class Dbm{
 		public void onCreate(SQLiteDatabase db){
 			// create database
 //			db_existance = DbmDBExistance.Exist;
+			Log.d("OpenHelper.onCreate",">>onCreate");
 			try{
-				db.execSQL("CREATE TABLE ");
+				db.execSQL("CREATE TABLE table-a ( _id INTEGER PRIMARY KEY );");
 			}catch(Exception e){
 				Log.e("aa","bb");
 			}
+			Log.d("OpenHelper.onCreate","<<onCreate");
 		}
 
 		/**
@@ -67,20 +69,49 @@ public class Dbm{
 	
 	public boolean open(){
 		// create OpenHelper
+		Log.d("Dbm.open",">>>open");
 		OpenHelper = new DbmOpenHelper( context );
 		db = OpenHelper.getWritableDatabase();
+		if( db == null ){
+			Log.e("DbmOpenHelper.open","db == null");
+			return false;
+		}
 		status = DbmStatus.Opened;
+		db_existance = DbmDBExistance.Exist;
+		Log.d("Dbm.open", "<<<open");
 		return (db != null);
 	}
 	
 	public void close(){
+		Log.d("Dbm.close",">>>close");
 		status = DbmStatus.Closed;
+		Log.d("Dbm.close","<<<close");
 	}
 	
-	public Cursor query(String sql){
+	public void execSQL(String sql){
+		Log.d("Dbm.execSQL",">>>execSQL");
+		try{
+			db.execSQL(sql);
+		}catch(Exception e){
+			Log.e("Dbm.execSql",e.getMessage());
+		}
+		Log.d("Dbm.execSQL","<<<execSQL");
+	}
+	
+	public Cursor rawQuery(String sql){
+		Cursor c;
+		Log.d("Dbm.rawQuery",">>>query");
 		if( db_existance == DbmDBExistance.Exist ){
-			return db.rawQuery(sql, null);
+			try{
+				c = db.rawQuery(sql, null);
+			}catch( Exception e ){
+				Log.e("Dbm.rawQuery", e.getMessage());
+				return null;
+			}
+			Log.d("Dbm.rawQuery","<<<query");
+			return c;
 		}else{
+			Log.d("Dbm.query","<<<query");
 			return null;
 		}
 	}
--- workspace/project001/src/project001/project/project001.java
+++ workspace/project001/src/project001/project/project001.java
@@ -5,7 +5,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import project001.project.Dbm;
 import android.widget.*;
-import java.lang.*;
+import android.database.*;
 
 public class project001 extends Activity {
     /** Called when the activity is first created. */
@@ -13,12 +13,55 @@ public class project001 extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
-        Log.e("tag","message");
+        Log.d("project001.onCreate",">new Dbm");
         Dbm dbm = new Dbm(getBaseContext());
-        Log.e("tag","open");
+        Log.d("project001.onCreate","<new Dbm");
+
+        Log.d("project001.onCreate",">Dbm.open");
         dbm.open();
-        dbm.query("create table test-table (_ID Integer auto inclemental)");
+        Log.d("project001.onCreate","<Dbm.open");
+
+        Log.d("project001.onCreate",">Dbm.query");
+        dbm.execSQL("create table test_table (_id integer primary key, f1 integer, f2 char(30) );");
+        Log.d("project001.onCreate","<Dbm.query");
+
+        Log.d("project001.onCreate",">Dbm.query2");
+        dbm.execSQL("insert into test_table ( f1, f2 ) values ( 1, 'android' );");
+        dbm.execSQL("insert into test_table ( f1, f2 ) values ( 2, 'android' );");
+        dbm.execSQL("insert into test_table ( f1, f2 ) values ( 3, 'android' );");
+        dbm.execSQL("insert into test_table ( f1, f2 ) values ( 4, 'android' );");
+        dbm.execSQL("insert into test_table ( f1, f2 ) values ( 5, 'android' );");
+        Log.d("project001.onCreate","<Dbm.query2");
+
+        Cursor c;
+        Log.d("project001.onCreate",">Dbm.query3");
+        c = dbm.rawQuery("select * from test_table;");
+        Log.d("project001.onCreate","<Dbm.query3");
+
+        if( c != null ){
+	
+	        Log.d("project001.onCreate", "count:"+c.getCount());
+	        String str[] = c.getColumnNames();
+	        for (int n = 0; n < c.getColumnCount(); n++){
+	        	Log.d("project001.onCreate", "CulumnNames:"+str[n]);
+	        }
+	        while( c.moveToNext() ){
+		        for (int n = 0; n < c.getColumnCount(); n++){
+			        for (int m = 0; m < c.getColumnCount(); m++){
+			        	Log.d("project001.onCreate", str[m]+":"+c.getString(m));
+			        }
+		        }
+	        }
+        }
+        
+        Log.d("project001.onCreate", ">Dbm.query4");
+        dbm.execSQL("drop table test_table;");
+        Log.d("project001.onCreate", "<Dbm.query4");
+        
+        Log.d("project001.onCreate",">Dbm.close");
         dbm.close();
+        Log.d("project001.onCreate","<Dbm.close");
+        
         Toast.makeText(getBaseContext(), "toast-test", Toast.LENGTH_LONG).show();
         
         setContentView(R.layout.main);
